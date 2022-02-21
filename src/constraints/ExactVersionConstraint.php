11446c19bc3b9011a1940ae2351981943a7d981b
Arne Blankerts <Arne@Blankerts.de>
2022-02-21 01:45:58 +0100

    Run CS-Fixer

--- src/constraints/ExactVersionConstraint.php
+++ src/constraints/ExactVersionConstraint.php
@@ -12,9 +12,11 @@ namespace PharIo\Version;
 class ExactVersionConstraint extends AbstractVersionConstraint {
     public function complies(Version $version): bool {
         $other = $version->getVersionString();
+
         if ($version->hasBuildMetaData()) {
             $other .= '+' . $version->getBuildMetaData()->asString();
         }
+
         return $this->asString() === $other;
     }
 }
